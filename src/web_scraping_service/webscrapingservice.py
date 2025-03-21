62279a817054506517f495a1de7d744620022c91
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-03-20 22:02:51 -0300

    Método obter_lista_sites

--- src/web_scraping_service/webscrapingservice.py
+++ src/web_scraping_service/webscrapingservice.py
@@ -1,3 +1,5 @@
+from typing import Generator
+
 import bs4
 import requests
 from src.web_scraping_service.iwebscarpingservice import IWebScrapingService, T
@@ -13,3 +15,20 @@ class WebScrapingService(IWebScrapingService[bs4.BeautifulSoup]):
         html = response.text
         soup = bs4.BeautifulSoup(html, 'html.parser')
         return soup
+
+    def obter_lista_sites(self) -> Generator[str, None, None]:
+        sites = self.__soup.find_all('li')
+
+        lista_sites = [
+            link['href']
+            for site in sites
+            if isinstance(site, bs4.Tag)
+               and (link := site.find("a"))
+               and isinstance(link, bs4.Tag)
+               and 'href' in link.attrs
+               and isinstance(link['href'], str)
+               and link['href'].startswith('https://')
+        ]
+
+        yield from lista_sites
+
