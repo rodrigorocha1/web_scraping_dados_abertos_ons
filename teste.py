6b41e38ba4f9e3c1d229c56d71dcb757e4e46ef9
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-03-22 00:13:07 -0300

    Teste pandas e polars

--- teste.py
+++ teste.py
@@ -1,30 +1,11 @@
-import pyodbc
-from dotenv import load_dotenv
-import os
+import polars as pl
 
-load_dotenv()
-
-SERVER = os.environ['SERVER']
-DATABASE = os.environ['DATABASE']
-USERNAME = os.environ['USERSQL']
-PASSWORD = os.environ['PASSWORD']
-
-connectionString = (
-    f"DRIVER={{ODBC Driver 18 for SQL Server}};"
-    f"SERVER={SERVER};DATABASE={DATABASE};"
-    f"UID={USERNAME};PWD={PASSWORD};"
-    f"TrustServerCertificate=yes"
+# Lendo o arquivo CSV com Polars
+df = pl.read_csv(
+    'https://ons-aws-prod-opendata.s3.amazonaws.com/dataset/balanco_energia_subsistema_ho/BALANCO_ENERGIA_SUBSISTEMA_2000.csv',
+    separator=';'
 )
-
-conn = pyodbc.connect(connectionString)
-print("Conexão bem-sucedida!", conn)
-
-with conn.cursor() as cursor:
-    cursor.execute("""
-        SELECT GETDATE();
-    """)
-
-    result = cursor.fetchone()
-    print(result)
-    print("Data e hora do servidor:", result[0])
-
+print(df)
+print(df.dtypes)
+df = df.to_pandas()
+print(df.info())
