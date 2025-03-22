b21fde50ef19cfed7ee92dfd93adf78de23e6840
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-03-22 00:24:29 -0300

    Correção print

--- webscrapingpipeline.py
+++ webscrapingpipeline.py
@@ -10,7 +10,7 @@ class WebScrapingPipeline:
     def rodar_web_scraping(self):
         flag, dados_site = self.__service_web_scraping.conectar_url()
         for link in self.__service_web_scraping.obter_lista_sites(dados_site=dados_site):
-            print(self.__service_web_scraping.url)
+
             self.__service_web_scraping.url = link
             print(self.__service_web_scraping.url)
 
