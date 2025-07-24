e7a141b085bb9671f243a7684479b98195e4f81a
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-07-23 22:22:47 -0300

    Método para checar conexão

--- src/banco_service/operacoes/i_operacao.py
+++ src/banco_service/operacoes/i_operacao.py
@@ -2,6 +2,11 @@ from abc import ABC, abstractmethod
 
 
 class IOperacao(ABC):
+
+    @abstractmethod
+    def checar_conexao(self) -> True:
+        pass
+
     @abstractmethod
     def salvar_consulta(self):
         pass
