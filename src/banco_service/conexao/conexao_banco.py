e884d9e6f9693519049c30db5d94aac73b9b55c7
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-07-23 22:39:23 -0300

    Revisão Tipagem

--- src/banco_service/conexao/conexao_banco.py
+++ src/banco_service/conexao/conexao_banco.py
@@ -1,4 +1,4 @@
-from typing import Any, TypeVar
+from typing import  TypeVar
 from src.banco_service.conexao.idb_config import IDBConfig
 from src.banco_service.conexao.db_confg_mysql import DbConfigMySQL
 from src.banco_service.conexao.iconexao import IConexao
