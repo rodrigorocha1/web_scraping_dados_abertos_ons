afb05ea8ed255951836f707161f0a56bf8d2e092
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-07-27 19:51:01 -0300

    Separando chaves de log

--- src/config/config.py
+++ src/config/config.py
@@ -8,7 +8,11 @@ class Config:
     SERVER = os.environ['SERVER']
     PORTA = os.environ['PORTA']
     USERSQL = os.environ['USERSQL']
-    PASSWORD =  os.environ['SENHA']
+    PASSWORD = os.environ['SENHA']
     DATABASE = os.environ['DATABASE']
 
-
+    SERVER_LOG = os.environ['SERVER_LOG']
+    PORTA_LOG = os.environ['PORTA_LOG']
+    USERSQL_LOG = os.environ['USERSQL_LOG']
+    PASSWORD_LOG = os.environ['PASSWORD_LOG']
+    DATABASE_LOG = os.environ['DATABASE_LOG']
