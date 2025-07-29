5afd2cf5c82f99cc52905250474f967a8f11af01
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-07-28 22:54:52 -0300

    Removendo imports

--- handler_main_pipeline.py
+++ handler_main_pipeline.py
@@ -2,11 +2,8 @@ from src.banco_service.operacoes.operacao_mysql import OperacaoMysql
 from src.handler_pipeline.checar_conexao_banco_handler import ChecarConexaoBancoHandler
 from src.banco_service.conexao.conexao_banco import ConexaoBanco
 from src.banco_service.conexao.db_confg_mysql import DbConfigMySQL
-
 from mysql.connector.connection import MySQLConnection
 from src.contexto.contexto_pipeiine import ContextoPipeline
-from collections import deque
-
 from src.handler_pipeline.checar_conexao_url_ons import ChecarConexaoUrlOns
 from src.handler_pipeline.coletar_links_csv_handler import ColetarLinksCSVHander
 from src.web_scraping_service.webscrapingbs4service import WebScrapingBS4Service
