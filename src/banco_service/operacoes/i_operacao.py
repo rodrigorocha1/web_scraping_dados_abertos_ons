4fe6043e413faa8f6e4256c4414d1f1b2d26f998
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-07-29 22:05:29 -0300

    métodos abstratos

--- src/banco_service/operacoes/i_operacao.py
+++ src/banco_service/operacoes/i_operacao.py
@@ -4,10 +4,10 @@ from typing import Any
 
 class IOperacao(ABC):
 
-
     @abstractmethod
     def salvar_consulta(self, sql: str, param: Any):
         pass
 
+    @abstractmethod
     def salvar_em_lote(self, sql: str, param: Any):
         pass
