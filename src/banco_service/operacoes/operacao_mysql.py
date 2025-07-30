507e5aa8b30296ac7d8370a5e00976e4e7067f56
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-07-29 21:55:06 -0300

    Tratando import circular

--- src/banco_service/operacoes/operacao_mysql.py
+++ src/banco_service/operacoes/operacao_mysql.py
@@ -2,7 +2,7 @@ from typing import Tuple, Any, List
 from mysql.connector.connection import MySQLConnection
 from src.banco_service.conexao.iconexaobanco import IConexaoBanco
 from src.banco_service.operacoes.i_operacao import IOperacao
-from src.utlis.llog_factory import logger
+
 
 
 class OperacaoMysql(IOperacao):
@@ -11,6 +11,7 @@ class OperacaoMysql(IOperacao):
         self.__conexao = conexao
 
     def salvar_consulta(self, sql: str, param: Tuple[Any, ...]):
+        from src.utlis.llog_factory import logger
         try:
             with self.__conexao as conn:
                 cursor = conn.cursor()
@@ -28,6 +29,7 @@ class OperacaoMysql(IOperacao):
             )
 
     def salvar_em_lote(self, sql: str, param: List[Tuple[Any, ...]]):
+        from src.utlis.llog_factory import logger
         try:
             with self.__conexao as conn:
                 cursor = conn.cursor()
